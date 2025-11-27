4ec871cdf9187779a02ca5172030babd17a81b24
Mike Chao <mike.chao.one@gmail.com>
2025-11-27 14:45:14 -0800

    Added lo extension

--- apps/local-rag/src/workers/db.worker.ts
+++ apps/local-rag/src/workers/db.worker.ts
@@ -1,13 +1,14 @@
 import { PGlite } from "@electric-sql/pglite"
 import { worker } from "@electric-sql/pglite/worker"
 import { vector } from "@electric-sql/pglite/vector"
+import { lo } from '@electric-sql/pglite/contrib/lo';
 import { OpfsAhpFS } from '@electric-sql/pglite/opfs-ahp'
 
 worker({
   async init() {
     return new PGlite({
       fs: new OpfsAhpFS("local-rag"),
-      extensions: { vector },
+      extensions: { vector, lo },
       relaxedDurability: true,
     })
   },
