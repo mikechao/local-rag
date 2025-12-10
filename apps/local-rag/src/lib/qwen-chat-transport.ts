ef849bb70461cbf053863b8e98ccfef1f351a84b
Mike Chao <mike.chao.one@gmail.com>
2025-12-10 12:37:11 -0800

    Added retrievalTool

--- apps/local-rag/src/lib/qwen-chat-transport.ts
+++ apps/local-rag/src/lib/qwen-chat-transport.ts
@@ -6,19 +6,38 @@ import {
     convertToModelMessages,
     streamText,
     wrapLanguageModel, 
-    extractReasoningMiddleware 
+    extractReasoningMiddleware,
+    tool,
+    Tool
 } from "ai";
 import { getQwenModel } from "./models/qwenModel";
 import { LocalRAGMessage } from "./local-rag-message";
+import { z } from "zod";
+import { retrieveChunks } from "./retrieval";
 
 export class QwenChatTransport implements ChatTransport<LocalRAGMessage> {
 
-  private wrappedModel: LanguageModel
+  private model: LanguageModel
+  private retrievalTool: Tool
 
   constructor() {
-    this.wrappedModel = wrapLanguageModel({
+    this.model = wrapLanguageModel({
       model: getQwenModel(),
       middleware: extractReasoningMiddleware({ tagName: 'think'})
+    })
+    this.retrievalTool = tool({
+      title: 'Document Retrieval',
+      description: 'Use this tool to retrieve relevant documents to answer user queries.',
+      inputSchema: z.object({
+        query: z.string().describe('The user query to retrieve documents for.'),
+      }),
+      execute: async ({ query }) => {
+        console.log('Retrieving documents for query:', query);
+        const retrievalResult = await retrieveChunks(query);
+        if (retrievalResult.results.length === 0) {
+          return 'No relevant documents found.';
+        }
+        return retrievalResult.results.map((r) => r.text).join('\n\n');}
     })
   }
 
@@ -31,8 +50,12 @@ export class QwenChatTransport implements ChatTransport<LocalRAGMessage> {
   ): Promise<ReadableStream<UIMessageChunk>> {
     const { messages, abortSignal } = options;
     const stream = streamText({
-      model: this.wrappedModel,
+      model: this.model,
+      system: `You are an AI assistant that helps users by providing accurate and concise information. Use the 'document-retrieval' tool to assist in answering user queries effectively about Stargate Atlantis.`,
       messages: convertToModelMessages(messages),
+      tools: {
+        'document-retrieval': this.retrievalTool,
+      },
       abortSignal,
     })
     return stream.toUIMessageStream({
